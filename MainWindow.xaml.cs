18de1c16ecb92cea2881c8050676319fab31e1d5
Chikipooki <homesotelittle@gmail.com>
2022-04-26 07:26:40 +0700

    Added display of the next block

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -97,10 +97,18 @@ namespace TetrisWPF
             }
         }
 
+        private void DrawNextBlock(BlockQueue blockQueue)
+        {
+            Block next = blockQueue.NextBlock;
+            NextImage.Source = blockImages[next.Id];
+
+        }
+
         private void Draw(GameState gameState)
         {
             DrawGrid(gameState.GameGrid);
             DrawBlock(gameState.CurrentBlock);
+            DrawNextBlock(gameState.BlockQueue);
         }
 
         private async Task GameLoop()
