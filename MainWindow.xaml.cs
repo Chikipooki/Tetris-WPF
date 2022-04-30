a3f77ecb5580552fc83bf74583acd8d3a0e33a38
Chikipooki <homesotelittle@gmail.com>
2022-04-30 14:01:34 +0700

    Added Hold block dispaly

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -104,11 +104,24 @@ namespace TetrisWPF
 
         }
 
+        private void DrawHeldBlock(Block heldBlock)
+        {
+            if (heldBlock == null)
+            {
+                HoldImage.Source = blockImages[0];
+            }
+            else
+            {
+                HoldImage.Source = blockImages[heldBlock.Id];
+            }
+        }
+
         private void Draw(GameState gameState)
         {
             DrawGrid(gameState.GameGrid);
             DrawBlock(gameState.CurrentBlock);
             DrawNextBlock(gameState.BlockQueue);
+            DrawHeldBlock(gameState.HeldBlock);
             ScoreText.Text = $"Score: {gameState.Score}";
         }
 
